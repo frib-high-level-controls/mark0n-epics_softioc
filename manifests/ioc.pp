8d29a6fa4787b92589422b70b6bffcfef77f63d4
Martin Konrad <konrad@frib.msu.edu>
2017-02-22 16:22:04 -0500

    Use make --question instead of --dry-run
    Exit code behavior is not specified by the man page for --dry-run.
    It is more intended for printing the commands that would be executed
    whereas --question is all about the exit code.

--- manifests/ioc.pp
+++ manifests/ioc.pp
@@ -105,7 +105,7 @@ define epics_softioc::ioc(
     exec { "build IOC ${name}":
       command => '/usr/bin/make',
       cwd     => $abstopdir,
-      unless  => '/usr/bin/make --dry-run',
+      unless  => '/usr/bin/make --question',
       require => Class['epics_softioc::software'],
     }
   }
