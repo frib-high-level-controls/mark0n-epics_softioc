e7efa1c3061f477af76759f3812fdbfc8c9ae3c5
Martin Konrad <konrad@frib.msu.edu>
2018-09-24 14:19:38 -0400

    Use snake_case for variables in Ruby code
    This makes "pdk validate" happy.

--- lib/facter/initsystem.rb
+++ lib/facter/initsystem.rb
@@ -1,8 +1,8 @@
 require 'facter'
 
-initExe = File.readlink('/proc/1/exe')
-initSystem = File.basename(initExe)
+init_exe = File.readlink('/proc/1/exe')
+init_system = File.basename(init_exe)
 
-Facter.add(:initsystem) {
-  setcode { initSystem }
-}
+Facter.add(:initsystem) do
+  setcode { init_system }
+end
